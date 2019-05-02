b773a0208ca72bea4ab866d6f721d30f213dd746
Stefaan De Cock <stefaan.decock@hogent.be>
2019-05-02 16:38:36 +0200

    Oefeningen

---
+++ FilmBrowser/js/filmBrowser.js
@@ -0,0 +1,165 @@
+function fetchRequest(url){
+	return fetch(url)
+  		.then(body => body.json()) 
+}
+
+class Film {
+  constructor(id, title, type, poster, year) {
+    this.id = id;
+    this.title = title;
+    this.poster = poster;
+    this.type = type;
+    this.year = year;
+    this.detail = {time:'',genre:'',director:'',actors:'',plot:'',language:''}
+  }
+  get id() { return this._id; }
+  set id(value) { this._id = value; }
+
+  get title() { return this._title; }
+  set title(value) { this._title = value; }
+
+  get type() { return this._type; }
+  set type(value) { this._type = value; }
+
+  get poster() { return this._poster; }
+  set poster(value) { value !== 'N/A' ? this._poster = value : this.poster = 'images/No_image_available.svg'; }
+ 
+  get year() { return this._year; }
+  set year(value) { this._year = value; }
+
+  get detail() { return this._detail; }
+  set detail(value) { this._detail = value; }
+
+
+}
+
+class FilmRepository {
+  constructor() {
+    this.films = [];
+  }
+
+  get films() { return this._films; }
+  set films(value) { this._films = value; }
+
+  addFilms(arrFilms) {
+    this.films = arrFilms.map(obj=>new Film(obj.imdbID, obj.Title, obj.Type, obj.Poster, obj.Year));
+  }
+  addDetail(id,objDetail){
+    const film = this.getFilmById(id);
+    film.detail.time = objDetail.Runtime;
+    film.detail.genre = objDetail.Genre;
+    film.detail.director = objDetail.Director;
+    film.detail.actors = objDetail.Actors;
+    film.detail.plot = objDetail.Plot;
+    film.detail.language = objDetail.Language;
+  }
+  getFilmById(id){
+    return this.films.find((f)=>f.id === id);
+  }
+}
+
+class FilmBrowserApp {
+  constructor() {
+    this.filmRepository = new FilmRepository();
+  }
+
+  get filmRepository() { return this._filmRepository; }
+  set filmRepository(value) { this._filmRepository = value; }
+
+  searchFilms(searchText){
+      if (searchText !== '' && /([^\s])/.test(searchText)){
+        fetchRequest(`http://www.omdbapi.com/?s=${searchText}&apikey=57927523`)
+        .then(resultValue => {
+          if (resultValue.Response === 'True'){
+			        this.filmRepository.addFilms(resultValue.Search);
+			        this.showFilms();
+          }
+		    })
+		    .catch(rejectValue => { console.log(rejectValue); });
+      }
+  }
+
+  getFilm(id){
+    fetchRequest(`http://www.omdbapi.com/?i=${id}&plot=full&apikey=57927523`)
+        .then(resultValue => {
+          //console.log(resultValue);
+          if (resultValue.Response === 'True'){
+              this.filmRepository.addDetail(id,resultValue);
+              const film = this.filmRepository.getFilmById(id);
+			        //this.filmRepository.addFilms(resultValue.Search);
+              //console.log(this.filmRepository.films)
+			        this.showDetailFilm(film);
+          }
+		    })
+		    .catch(rejectValue => { console.log(rejectValue); });
+  }
+  
+
+  showFilms() {
+    document.getElementById('films').innerHTML = '';
+    this.filmRepository.films.forEach((film) => {
+      document.getElementById('films').insertAdjacentHTML('beforeend',
+      `     
+        <div class="col s12 m6">
+          <div class="card small horizontal">
+            <div class="card-image">
+              <img src="${film.poster}">
+              </div>
+            <div class="card-stacked">  
+              <div class="card-content">
+                <span class="card-title">${film.title}</span>             
+                <ul>
+                  <li>Type: ${film.type}</li>
+                  <li>Year: ${film.year}</li>
+                </ul>
+                <a id="${film.id}" class="btn-floating btn-small waves-effect waves-light green"><i class="material-icons">details</i></a>
+              </div> 
+            </div>       
+          </div>
+        </div>
+      `
+      );
+      document.getElementById(film.id).onclick = ()=>{
+        this.getFilm(film.id);
+      }
+    });
+  }
+
+  showDetailFilm(film){
+    let details = '';
+    Object.entries(film.detail).forEach(([key, value]) => {
+       details += `<li><label>${key}:</label> ${value}</li>`;
+    });
+    document.getElementById('films').innerHTML = '';    
+    document.getElementById('films').insertAdjacentHTML('beforeend',
+      `     
+        <div class="col s12">
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="${film.poster}">
+            </div>
+            <div class="card-stacked">  
+              <div class="card-content">
+                <span class="card-title">${film.title}</span>             
+                <ul>
+                  <li><label>type:</label> ${film.type}</li>
+                  <li><label>year:</label> ${film.year}</li>
+                  ${details}
+                </ul>
+              </div> 
+            </div>       
+          </div>
+        </div>
+      `
+    );
+  }
+}
+
+const init = function() {
+  const filmBrowserApp = new FilmBrowserApp();
+  document.getElementById("searchBtn").onclick = ()=>{
+    filmBrowserApp.searchFilms(document.getElementById('searchText').value)
+  }
+}
+
+window.onload = init;
---
+++ Trivia/js/trivia.js
@@ -0,0 +1,171 @@
+function fetchRequest(url){
+	return fetch(url)
+  		.then(body => body.json()) //retourneert promise object met response
+}
+class Trivia {
+	constructor(category, difficulty, question, answers, correctAnswer) {
+		this.category = category;
+		this.difficulty = difficulty;
+		this.question = question;
+		this.answers = answers;
+		this.correctAnswer = correctAnswer
+	}
+	get category() {
+		return this._category;
+	}
+	set category(value) {
+		this._category = value;
+	}
+	get difficulty() {
+		return this._difficulty;
+	}
+	set difficulty(value) {
+		this._difficulty = value;
+	}
+	set question(value) {
+		this._question = value;
+	}
+	get question() {
+		return this._question;
+	}
+	set answers(value) {
+		this._answers = value;
+	}
+	get answers() {
+		return this._answers;
+	}
+	set correctAnswer(value) {
+		this._correctAnswer = value;
+	}
+	get correctAnswer() {
+		return this._correctAnswer;
+	}
+	
+}
+class TriviaRepository {
+  	constructor() {
+    	this._triviaObjects = [];
+		this.currentTrivia = 0;
+		this.correctAnswers = 0;
+  	}
+
+  	get triviaObjects() { return this._triviaObjects; }
+	get currentTrivia() {
+		return this._currentTrivia;
+	}
+	set currentTrivia(value) {
+		this._currentTrivia = value;
+	}
+	get correctAnswers() {
+		return this._correctAnswers;
+	}
+	set correctAnswers(value) {
+		this._correctAnswers = value;
+	}
+	get numberOfTrivias(){
+		return this._triviaObjects.length;
+	}
+
+  	addTriviaObjects(dataObjects) {
+		  this._triviaObjects = 
+		         dataObjects.map(t=>new Trivia(t.category,t.difficulty,t.question,[...t.incorrect_answers,t.correct_answer],t.correct_answer));
+  	}
+
+  	getNextTrivia(){
+		this.currentTrivia++;
+		this.triviaObjects[this.currentTrivia-1].answers.sort();
+		return this.triviaObjects[this.currentTrivia-1];
+	}
+	
+	checkAnswer(answer){
+		if (this.triviaObjects[this.currentTrivia-1].correctAnswer === answer){
+			this.correctAnswers++;
+			return true;
+		}
+		else return false;
+	}
+	checkEndGame(){
+		return this.currentTrivia === this.numberOfTrivias;
+	} 
+}
+class TriviaApp {
+	constructor() {
+		this.triviaRepository = new TriviaRepository();		
+		this.getData();		
+	}
+	get triviaRepository() {
+		return this._triviaRepository;
+	}
+	set triviaRepository(value) {
+		this._triviaRepository = value;
+	}
+	
+	getData(){
+		fetchRequest('https://opentdb.com/api.php?amount=10')
+  		.then(resultValue => {
+			const trColl = resultValue.results;
+			this.triviaRepository.addTriviaObjects(trColl);
+			this.showTrivia();
+		})
+		.catch(rejectValue => { console.log(rejectValue); });
+	}
+	showTrivia(){
+		const trivia = this.triviaRepository.getNextTrivia();
+		const triviaHTML = document.getElementById("trivia");
+		triviaHTML.innerHTML = '';
+		document.getElementById("number").innerText= `Question: ${this.triviaRepository.currentTrivia}/${this.triviaRepository.numberOfTrivias}`;
+		triviaHTML.insertAdjacentHTML('beforeend',
+			`<div class="card-content">
+				<span class="card-title">${trivia.category} - Difficulty: ${trivia.difficulty}</span>
+				<p>${trivia.question}</p>
+			</div>`
+		);
+		const divCA = document.createElement('div');
+		divCA.className = 'card-action';
+		const divRow = document.createElement('div');
+		divRow.className = 'row';
+		trivia.answers.forEach((a) => {
+			divRow.insertAdjacentHTML('beforeend',
+				`<div class="col s12 m6">
+					<p>
+						<label>
+							<input class="with-gap" name="group" value="${a}" type="radio" />
+							<span>${a}</span>
+						</label>
+					</p>
+				</div>`
+			)
+		});
+		divCA.appendChild(divRow);
+		triviaHTML.appendChild(divCA);	
+			triviaHTML.insertAdjacentHTML('beforeend',
+				`<div class="card-action">
+					<a id="next" class="btn-small blue darken-2">Submit answer<i class="material-icons right">send</i></a>
+				</div>`		
+			)
+			document.getElementById('next').onclick = () => {
+			triviaHTML.insertAdjacentHTML('beforeend',
+				`<div class="card-action">
+					<p>Answer: ${trivia.correctAnswer}</p>
+				</div>`		
+				);
+			this.triviaRepository.checkAnswer(document.querySelector('input[name="group"]:checked').value);
+			document.getElementById("correct").innerText= `Correct answers: ${this.triviaRepository.correctAnswers}/${this.triviaRepository.numberOfTrivias}`;
+			if (!this._triviaRepository.checkEndGame()){
+				document.getElementById('next').innerText = 'Next';
+				document.getElementById('next').onclick = ()=>{
+					this.showTrivia();
+				};
+			}
+			else{
+				document.getElementById('next').className = 'btn-small blue darken-2 disabled';
+			};
+		}
+	}
+}
+
+const init = function(){
+	const app = new TriviaApp();
+}
+
+window.onload = init;
